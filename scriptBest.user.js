c3803c4e13522aecab6cd61080700370e47dbeb7
tryusweb <57223113+tryusweb@users.noreply.github.com>
2019-11-11 10:43:34 +0100

    Update scriptBest.user.js

--- scriptBest.user.js
+++ scriptBest.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @version 7.3.6
+// @version 7.3.8
 // @name scriptBest
-// @description OGame: Miglioramento giocabilità con funzioni automatiche e di controllo
+// @description OGame: Miglioramento giocabilità  con funzioni automatiche e di controllo
 // @author info@tryus.it
 // @creator Tryus
 // @copyright 2017, Tryus
@@ -20,52 +20,32 @@
 
 (function() {
     let debug = false;
-    function addScript(name){
-        let headers = unsafeWindow.debugScriptBest ? {
+    function addScript(b) {
+        let a = unsafeWindow.debugScriptBest ? {
             'Cache-Control': 'no-store, no-cache, must-revalidate, max-age=0'
         } : {};
-
         GM_xmlhttpRequest({
             method: 'GET',
-            url: unsafeWindow.pathDomain+"/jsBot/js/"+unsafeWindow.versionSBI+"/"+name+".js",
+            url: unsafeWindow.pathDomain + '/jsBot/js/' + unsafeWindow.versionSBI + '/' + b + '.js',
             ignoreCache: unsafeWindow.debugScriptBest,
-            headers: headers,
-            onload: function (js) {
-                let script = document.createElement("script");
-                script.innerHTML = js.responseText;
-                document.body.appendChild(script);
+            headers: a,
+            onload: function(b) {
+                let a = document.createElement('script');
+                a.innerHTML = b.responseText, document.body.appendChild(a);
             },
-            onerror: function(resp){
-                if(resp && resp.error && resp.error.indexOf("404") !== -1) {
-                    if(!unsafeWindow.sendErrorMessage){
-                        unsafeWindow.sendErrorMessage = 1;
-                        alert("This version is not supported, please update the bot by downloading it from the site: "+unsafeWindow.pathDomain);
-                    }
-                }
+            onerror: function(a) {
+                a && a.error && a.error.indexOf('404') !== -1 && !unsafeWindow.sendErrorMessage && (unsafeWindow.sendErrorMessage = 1, alert('This version is not supported, please update the bot by downloading it from the site: ' + unsafeWindow.pathDomain));
             }
         });
     }
-
-    if(location.href.indexOf("api") === -1 && location.href.indexOf("board") === -1 && location.href.indexOf("allianceInfo") === -1) {
-
-        unsafeWindow.debugScriptBest = debug;
-        unsafeWindow.TOKEN_USER = GM_getValue('TOKEN_USER', null);
-        unsafeWindow.pathDomain = "https://scriptog.tryus.it";
-        unsafeWindow.webservicePath = unsafeWindow.pathDomain + "/ws/";
-        unsafeWindow.versionSBI = GM_info.script.version;
-        unsafeWindow.GM_xmlhttpRequest = GM_xmlhttpRequest;
-        unsafeWindow.GM_setValue = GM_setValue;
-        unsafeWindow.GM_getValue = GM_getValue;
-
-        addScript("AutoLoader");
-        addScript("Main");
-
-        let rI = setInterval(function(){
-            if(typeof AutoLoader !== "undefined" && typeof Main !== "undefined"){
+    if (location.href.indexOf('api') === -1 && location.href.indexOf('board') === -1 && location.href.indexOf('allianceInfo') === -1) {
+        unsafeWindow.debugScriptBest = debug, unsafeWindow.TOKEN_USER = GM_getValue('TOKEN_USER', null), unsafeWindow.pathDomain = 'https://scriptog.tryus.it', unsafeWindow.webservicePath = unsafeWindow.pathDomain + '/ws/', unsafeWindow.versionSBI = GM_info.script.version, unsafeWindow.GM_xmlhttpRequest = GM_xmlhttpRequest, unsafeWindow.GM_setValue = GM_setValue, unsafeWindow.GM_getValue = GM_getValue, addScript('AutoLoader'), addScript('Main');
+        let rI = setInterval(function() {
+            if (typeof AutoLoader !== 'undefined' && typeof Main !== 'undefined') {
                 clearInterval(rI);
-                let auto = new AutoLoader();
+                let a = new AutoLoader();
             }
-        },100);
+        }, 100);
     }
 
 })();
